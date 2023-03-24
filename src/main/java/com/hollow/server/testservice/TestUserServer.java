1b693c5b79c90a14c98840754e5e2289de6c8993
CokePrince <cokeprincepro@gmail.com>
2023-03-24 12:59:46 +0800

    hollow-server v1.0.2_20230323

---
+++ src/main/java/com/hollow/server/testservice/TestUserServer.java
@@ -0,0 +1,10 @@
+package com.hollow.server.testservice;
+
+/**
+ * @Description: java类作用描述
+ * @Author: jiangyong
+ * @CreateDate: 2023/3/22 14:43
+ * @Version: 1.0
+ */
+public class TestUserServer {
+}
---
+++ src/main/java/com/hollow/server/web/TestControll.java
@@ -0,0 +1,10 @@
+package com.hollow.server.web;
+
+/**
+ * @Description: java类作用描述
+ * @Author: jiangyong
+ * @CreateDate: 2023/3/22 14:29
+ * @Version: 1.0
+ */
+public class TestControll {
+}
